87fdd28ffb6895274bd60b361034aab16c08db00
Oszi77 <erdei.oszkar77@gmail.com>
2020-11-09 13:00:18 +0100

    Media queryk hozzáadása.

--- src/App.vue
+++ src/App.vue
@@ -1,71 +1,72 @@
 <template>
 
-   <div id="app">
-     <header>
-        <img class="logo" src="./assets/Logó.svg" alt="">
-      </header>
+<div id="app">
+<header>
+  <picture>
+    <img class="logo" src="./assets/Logó.svg" alt="Forcast info logó">
+  </picture>
+  <nav></nav>
+</header>
 
-    <main>
-      <p>{{req}}</p>
+<main>
+<p>{{req}}</p>
 
-      <div class="search-box">
-        <input 
-          type="text" 
-          class="search-bar" 
-          placeholder="Írj be egy várost ..."
-          v-model="query" 
-          @keypress="fetchWeather"
-          />
+<section class="search-box">
+  <input 
+    type="text" 
+    class="search-bar" 
+    placeholder="Írj be egy várost ..."
+    v-model="query" 
+    @keypress="fetchWeather">
+</section>
+
+<section>
+  <article class="forcast-box" v-if="typeof req.main != 'undefined'">
+    <div class="row">
+      <div class="loc-time">
+        <div class="location">{{req.name}}</div>
+        <div class="time">{{time}}</div>
       </div>
 
-      <div class="forcast-box" v-if="typeof req.main != 'undefined'">
-        <div class="row">
-          <div class="loc-time">
-            <div class="location">{{req.name}}</div>
-            <div class="time">{{time}}</div>
-          </div>
-          <div class="Date">
-            <div class="day">{{dating.day}}</div>
-            <div class="mounth">{{dating.mounth}}. {{dating.date}}</div>
-          </div>
-        </div>
-
-        <div class="row">
-          <div class="temperature">
-            <span id="ig">
-              <img src="./assets/Eső.png" alt="">
-            </span>
-            <span class="degree">{{Math.round(req.main.temp)}}°C</span>
-          </div>
-
-          <div class="weather">
-            <div class="state">{{req.weather[0].main}}</div>
-            <span class="humidity-img">
-              <img src="./assets/esernyő.png" alt="">
-            </span>
-            <span class="humidity">{{req.main.humidity}}%</span>
-            <span class="min">{{Math.round(req.main.temp_min)}}</span><span><img class="aditional-degree" src="./assets/arrow-down.svg" alt=""></span>
-            <span class="max">{{Math.round(req.main.temp_max)}}</span><span><img class="aditional-degree" src="./assets/arrow-up.svg" alt=""></span>
-          </div> 
-        </div>
-
+      <div class="Date">
+        <div class="day">{{dating.day}}</div>
+        <div class="mounth">{{dating.mounth}}. {{dating.date}}</div>
       </div>
-      <!-- <div class="fiveday-forcast-box">
-        <div class="loc-time">
-          <div class="location">{{req.name}}</div>
-          <div class="time">12:28<span>PM</span></div>
-        </div>
-      </div> -->
+    </div>
 
-     
-    </main>
+    <div class="row">
+      <div class="temperature">
+        <picture><img src="./assets/Eső.png" alt="Esőfelhő ikon"></picture>
+        <span class="degree">{{Math.round(req.main.temp)}}C°</span>
+      </div>
 
-    <footer>
-      <div>Created by Oszkár Design</div>
-    </footer>
+      <div class="weather">
+        <div class="state">{{req.weather[0].main}}</div>
+          <picture class="humidity-img"><img src="./assets/esernyő.png" alt="esernyő ikon"></picture>
+          <span class="humidity">{{req.main.humidity}}%</span>
+          <span class="min">{{Math.round(req.main.temp_min)}}<img class="aditional-degree" src="./assets/arrow-down.svg" alt="Legalacsonyabb hőmérséklet"></span>
+          <span class="max">{{Math.round(req.main.temp_max)}}<img class="aditional-degree" src="./assets/arrow-up.svg" alt="Legmagasabb hőmérséklet"></span>
+      </div> 
+    </div>
+  </article>
 
+<!-- 
+  <darticle class="fiveday-forcast-box">
+    <div class="loc-time">
+    <div class="location">{{req.name}}</div>
+      <div class="time">12:28<span>PM</span></div>
+    </div>
+  </article> 
+-->
+</section> 
+</main>
 
-   </div>
+<footer>
+  <div>Created by Oszkár Design</div>
+</footer>
+
+<!-- Az #app rész vége. -->
+</div>
 </template>
 
 <script>
@@ -139,50 +140,37 @@
      gsap.from('.logo', {opacity: 0, duration: 1, y: -50, ease: 'elastic(1, 0.3)'});
    },
 }
-
 </script>
 
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Open+Sans&display=swap');
-  *{
-    padding: 0;
-    margin: 0;
-    border: 0;
-    outline: none;
-    font-size: 14px;
-}
+
+*{padding: 0; margin: 0; border: 0; outline: none; font-size: 14px;}
 body{
   font-family: 'Open Sans', sans-serif;
   font-weight: 400;
   color: #fff;
 }
-
 #app{
   background-image: url('./assets/szarva-nap.jpg');
-  background-size: cover;
-  background-position: bottom;
+  background-size:cover;
+  background-attachment: fixed;
   transition: 0.4s;
 }
 
-main{
-  min-height: 1080px;
-  padding: 25px;
-}
 header{
   widows: 100%;
   background-color: rgba(255, 255, 255, 0.5);
   padding: 10px;
 
 }
-.search-box{
-  width: 100%;
-  
-}
+
 .search-box .search-bar{
   display: block;
-  max-width: 800px;
+  width: 42%;
   margin: 30px auto;
   padding: 15px;
+  font-family: 'Open Sans', sans-serif;
   font-size: 20px;
   font-style: italic;
   box-shadow: 3px 3px 6px rgba(0, 0, 0, 0.25);
@@ -193,22 +181,31 @@ header{
   box-shadow: 3px 3px 12px rgba(0, 0, 0, 0.25);
   background-color: rgba(255, 255, 255, 0.75);
 }
+
+main{
+  padding: 25px;
+}
 .forcast-box{
-  margin-left: 200px;
+  margin-left: 10%;
   max-width: 520px;
   max-height: 290px;
   padding: 30px;
   border-radius: 10px;
   background-color: rgba(255, 255, 255, 0.5);
   box-shadow: 3px 3px 6px rgba(0, 0, 0, 0.25);
+  font-family: 'Open Sans', sans-serif;
   font-style: italic;
   font-weight: 400;
 }
 .row{
   display: flex;
 }
+.loc-time{
+  width: 50%;
+}
 .loc-time .location{
   text-shadow: 3px 3px 6px rgba(0, 0, 0, 1);
+  font-family: 'Open Sans', sans-serif;
   font-size: 30px;
   font-weight: 600;
 }
@@ -217,7 +214,8 @@ header{
   font-size: 30px;
 }
 .Date{
-  margin-left: 330px;
+  width: 50%;
+  text-align: right;
 }
 .Date .day{
   text-shadow: 3px 3px 6px rgba(0, 0, 0, 1);
@@ -247,10 +245,6 @@ header{
 .weather .min, .max{
   margin-left: 30px;
 }
-.aditional-degree{
-  font-size: 30px;
-  color: #fff;
-}
 
 .fiveday-forcast-box{
   max-width: 1123px;
@@ -273,4 +267,40 @@ footer{
   font-weight: 600;
 }
 
+/** Media queryk képernyő beállításokhoz, hogy reszponzív legyen az
+*   adot felbontásoknál - 2020 -as felbontások.
+*/
+/** SM - small devices  
+@media only screen
+and (min-width:  576px;)
+and (max-width:  768px;){
+
+} - régebbi megoldás */
+
+/** SM - small devices - Mobil phone */
+@media (min-width:  576px)
+{
+  header {background-color: #f77300;}
+}
+/** MD - medium devices - Tablet  */
+@media (min-width:  768px){
+  header {background-color: #36f700;}
+  #app{
+    background-image: url('./assets/szarva-nap-768x432.jpg');
+  }
+  main{height: 432px}
+}
+/** LG - larg devices - Laptop  */
+@media (min-width:  992px){
+  header {background-color: #f72500;}
+  #app{
+    background-image: url('./assets/szarva-nap.jpg');
+  }
+  main{height: 1080px}
+}
+/** XG -extra lagr devices - Dekstop PC */
+@media (min-width:  1200px)
+{
+  header {background-color: #005ff7}
+}
 </style>
